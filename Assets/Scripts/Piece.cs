f4b0caf61bae9c33f59b3479707179fbd101ef3e
Luisa <lsaenzroa@gmail.com>
2025-06-03 18:31:42 -0500

    animation piece

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -27,19 +27,40 @@ public class Piece : MonoBehaviour
     {
         x = x_;
         y = y_;
-        board =  board_;
+        board = board_;
+        transform.localScale = Vector3.one * 0.35f;
+        transform.DOScale(Vector3.one, 0.35f);
+    }
+    public void Remove(bool animated)
+    {
+        if (animated)
+        {
+            transform.DORotate(new Vector3(0, 0, -120f), 0.12f);
+            transform.DOScale(Vector3.one * 1.2f, 0.085f).onComplete = () =>
+            {
+                transform.DOScale(Vector3.zero, 0.1f).onComplete = () =>
+                {
+                    Destroy(gameObject);
+                };
+            };
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    public void Move(int desX, int desY) 
+
+    public void Move(int desX, int desY)
     {
-        transform.DOMove(new Vector3 (desX, desY, -5f), 0.25f).SetEase(Ease.InOutCubic).onComplete = () => 
+        transform.DOMove(new Vector3(desX, desY, -5f), 0.25f).SetEase(Ease.InOutCubic).onComplete = () =>
         {
             x = desX;
             y = desY;
         };
-    } 
+    }
     [ContextMenu("Test Move")]
-    public void MoveTest () 
+    public void MoveTest()
     {
         Move(0, 0);
     }
